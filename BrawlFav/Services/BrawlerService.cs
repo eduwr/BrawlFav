0922ac4e417b496d0b6e89835f39fe41fecd7fe3
Eduardo Wronscki Ricardo <eduwric@gmail.com>
2023-08-31 12:41:28 -0300

    create service

--- BrawlFav/Services/BrawlerService.cs
+++ BrawlFav/Services/BrawlerService.cs
@@ -6,7 +6,15 @@ public static class BrawlerService
 {
     static List<Brawler> Brawlers { get; }
 
-    static BrawlerService() {  Brawlers = new List<Brawler>();}
+    static BrawlerService() { Brawlers = new List<Brawler>(); }
 
     public static List<Brawler> GetAll() => Brawlers;
+
+    public static Brawler Create()
+    {
+        Brawler brawl = new Brawler { Id = 1, Name = "test", StarPowers = new StarPower[0], Gadgets = new Gadget[0] };
+        Brawlers.Add(brawl);
+
+        return brawl;
+    }
 }
---
+++ BrawlFavTests/Services/BrawlerServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using BrawlFav.Models;
+using BrawlFav.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrawlFav.Services.Tests
+
+{
+    [TestClass]
+    public class BrawlerServiceTests
+    {
+        [TestMethod]
+        public void Create_AddsBrawlerToList()
+        {
+
+            var createdBrawler = BrawlerService.Create();
+
+            var brawlers = BrawlerService.GetAll();
+
+            Assert.AreEqual(1, brawlers.Count);
+            Assert.AreEqual(createdBrawler, brawlers.First());
+
+        }
+
+        [TestMethod]
+        public void Create_ReturnsCreatedBrawler()
+        {
+            var expectedId = 1;
+            var expectedName = "test";
+
+            var createdBrawler = BrawlerService.Create();
+
+            Assert.AreEqual(expectedId, createdBrawler.Id);
+            Assert.AreEqual(expectedName, createdBrawler.Name);
+            Assert.AreEqual(0, createdBrawler.StarPowers.Length);
+            Assert.AreEqual(0, createdBrawler.Gadgets.Length);
+        }
+    }
+}
